6b4633c4d59ffb0b88a6022241f878573d5f8a84
mapogolions <ivanovpavelalex45@gmail.com>
2021-03-03 23:39:56 +0500

    test parallel quick sort algo

--- QuickSort.Tests/QuickSortTests.cs
+++ QuickSort.Tests/QuickSortTests.cs
@@ -3,11 +3,42 @@ namespace QuickSort.Tests
     using Xunit;
     using QuickSort.Algorithm;
     using System.Collections.Generic;
+    using System;
 
     public class QuickSortTests
     {
+         [Fact]
+        public void ParallelQuickSort_NonEmptyList_ReturnsNewSortedList()
+        {
+            var source = new List<int> { -1, 2, -20, 0, 6 };
+            var actual = QuickSort.ParallelSort(source, Environment.ProcessorCount);
+
+            Assert.False(ReferenceEquals(source, actual));
+            Assert.Equal(new List<int> { -20, -1, 0, 2, 6 }, actual);
+        }
+
         [Fact]
-        public void QuickSort_NonEmptyList_ReturnsNewSortedList()
+        public void ParallelQuickSort_OneElementList_ReturnsNewListWithOneElement()
+        {
+            var source = new List<int> { 1 };
+            var actual = QuickSort.ParallelSort(source, Environment.ProcessorCount);
+
+            Assert.False(ReferenceEquals(source, actual));
+            Assert.Equal(source, actual);
+        }
+
+        [Fact]
+        public void ParallelQuickSort_EmptyList_ReturnsNewEmptyList()
+        {
+            var source = new List<int>();
+            var actual = QuickSort.ParallelSort(source, Environment.ProcessorCount);
+
+            Assert.False(ReferenceEquals(source, actual));
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public void SequentialQuickSort_NonEmptyList_ReturnsNewSortedList()
         {
             var source = new List<int> { -1, 2, -20, 0, 6 };
             var actual = QuickSort.Sort(source);
@@ -17,7 +48,7 @@ namespace QuickSort.Tests
         }
 
         [Fact]
-        public void QuickSort_OneElementList_ReturnsNewListWithOneElement()
+        public void SequentialQuickSort_OneElementList_ReturnsNewListWithOneElement()
         {
             var source = new List<int> { 1 };
             var actual = QuickSort.Sort(source);
@@ -27,7 +58,7 @@ namespace QuickSort.Tests
         }
 
         [Fact]
-        public void QuickSort_EmptyList_ReturnsNewEmptyList()
+        public void SequentialQuickSort_EmptyList_ReturnsNewEmptyList()
         {
             var source = new List<int>();
             var actual = QuickSort.Sort(source);
